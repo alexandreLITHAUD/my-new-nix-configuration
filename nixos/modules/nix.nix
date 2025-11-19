277f7c9accab2d6e5503d3c43b66e6800fabebf8
Alexandre LITHAUD <134046985+alexandreLITHAUD@users.noreply.github.com>
2025-11-19 15:36:51 +0100

    Add registry to nixos

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,4 +1,9 @@
-{
+{ inputs, ... }: {
+
+  nix.registry = {
+    nixpkgs.flake = inputs.nixpkgs;
+  };
+
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     flake-registry = "https://channels.nixos.org/flake-registry.json";
